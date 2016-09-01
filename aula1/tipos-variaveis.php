cadf8f98696e64075fe62f1af377cb9e63782675
tksprogramador <patrick.sk8.xt@hotmail.com>
2016-08-31 22:43:09 -0300

    Tipos De variaveis

---
+++ aula1/tipos-variaveis.php
@@ -0,0 +1,19 @@
+<?php 
+
+  $var = 56;
+  $var1 = 5.6;
+  $var2 = "5.6";
+  $var3 = TRUE;
+  $var4 = array("Patrick" , "gatao")
+
+  class nome{
+
+
+
+
+  }
+
+  $var5 = new Nome:
+  echo gettype($var4);
+
+ ?>
