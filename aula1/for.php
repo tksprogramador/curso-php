5f8fd4dc7176e53171e047ec2eb0b0e18c335786
tksprogramador <patrick.sk8.xt@hotmail.com>
2016-09-01 11:17:58 -0300

    IF E FOR NO PHP

---
+++ aula1/for.php
@@ -0,0 +1,24 @@
+
+
+<?php 
+//$idade = 17;
+//if ($idade > 18):
+//	echo "resultado";
+//elseif($idade == 17):
+//	echo "Resultado certo";
+//}else:
+//echo "resultado 2";
+//endif;
+
+//INCREMENTA
+//$numeros = 10;
+//for($i=0; $i <= $numeros ; $i++){
+//	echo $i."</br>";
+//}
+
+//DECREMENTA
+$numeros = 10;
+for($i=0; $i <= $numeros ; $numeros--){
+	echo $numeros."</br>";
+
+}
---
+++ aula1/if.php
@@ -0,0 +1,12 @@
+
+
+<?php 
+$idade = 17;
+if ($idade > 18):
+	echo "resultado";
+elseif($idade == 17):
+	echo "Resultado certo";
+}else:
+echo "resultado 2";
+endif;
+
