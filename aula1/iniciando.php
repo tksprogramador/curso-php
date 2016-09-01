62f273470eaf281c75740cb83030ee659bfe7f69
tksprogramador <patrick.sk8.xt@hotmail.com>
2016-08-31 22:31:40 -0300

    iniciando php

---
+++ aula1/iniciando.php
@@ -0,0 +1,20 @@
+<?php  
+
+$var = "Patrick";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Curso PHP</title>
+</head>
+<body>
+<p>
+	Olá , <?=$var ?>
+
+</p>
+	
+</body>
+</html>
