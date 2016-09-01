475e1394f30b2b818685f057434c75b4aa670ed8
tksprogramador <patrick.sk8.xt@hotmail.com>
2016-09-01 11:05:42 -0300

    constante em Php

---
+++ aula1/constante.php
@@ -0,0 +1,25 @@
+<?php 
+
+define('CSS', 'css/');
+define('JS', 'js/');
+
+ ?>
+
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+ 	<meta charset="UTF-8">
+ 	<title>Curso PHP</title>
+ 	<script src="<?php echo CSS."style.css" ;?>"  charset="utf-8"></script>
+
+ </head>
+ <body>
+ 	<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker. </p>
+
+
+
+ 	<!---->
+ 	 	<script src="<?php echo JS."js.js" ;?>" type="text/javascript"  charset="utf-8"></script>
+ </body>
+ </html>
